2db0cde75bf8be76adeb5431b757c378a8f6f1bf
FloMalv <97157788+FloMalv@users.noreply.github.com>
2022-04-17 16:40:30 +0200

    v5.4

--- Main.java
+++ Main.java
@@ -369,7 +369,7 @@ public class Main {
                         "<div class=\"infos_checks\">" +
                         "<label for=\"Mousqueton\">Mousqueton</label>" +
                         // vérifie si la personne a un mousquetton
-                        "<input class=\"box\" type=\"checkbox\" id=\"Mousqueton\" name=\"Mousqueton\"disabled=\"disabled\" + " + mousquetonCheck + " \n" +
+                        "<input type=\"checkbox\" id=\"Mousqueton\" name=\"Mousqueton\"disabled=\"disabled\" " + mousquetonCheck + " \n" +
 
 
 
